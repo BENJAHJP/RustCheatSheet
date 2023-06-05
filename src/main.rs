cec1657e31bfa05011dd2308a0bb0f42f04c29d0
b3njah <benjahjp@gmail.com>
2023-06-05 12:38:39 +0300

    strings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+mod strings;
+
 use std::io;
 use rand::Rng;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
---
+++ src/strings.rs
@@ -0,0 +1,12 @@
+fn strings(){
+    let mut st1: String = String::new();
+    st1.push('A');
+    st1.push_str("word");
+
+    for word in st1.split_whitespace(){
+        println!("{}", word);
+    }
+
+    let st2 = st1.replace("A", "Another");
+    println!("{}", st2);
+}
