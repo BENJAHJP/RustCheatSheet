50efd8181b5715be7769bf355b93a3147c9c89da
b3njah <benjahjp@gmail.com>
2023-06-05 11:55:34 +0300

    datatypes

---
+++ src/datatypes.rs
@@ -0,0 +1,10 @@
+fn main(){
+    println!("Max u32: {}", u32::Max);
+    println!("Max u32: {}", f32::Max);
+
+    let is_true: bool = true;
+    let my_grade: char = 'A';
+    let age: u32 = 67;
+    let PI: f32 = 3.2637532;
+    let name: &str = "Benjamin";
+}
